48474998216069db6efafb26e30bb96f9ca25716
Mrinmoy Biswas <mrinmoy@Mrinmoys-MacBook-Pro-M5.local>
2026-02-10 12:14:04 -0500

    BISWAS

--- Test1.py
+++ Test1.py
@@ -1,6 +1,17 @@
-print("Mrinmoy Biswas")
+from ragas.metrics._context_precision import LLMContextPrecisionWithoutReference
+
+
+# user_input = Query
+# response = response
+# reference = Ground Truth (Which we already know)
+# retrived_context =>  Top k retrieved docs
 
 
 def test_context_precision():
 
 
+# Create objects of the class for the specified matrix
+context_precision = LLMContextPrecisionWithoutReference()
+
+# Feed Data
+# Get the Score
