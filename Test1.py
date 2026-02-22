0984375ce5349edecebc39d7a346d9f5970a6925
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2026-02-22 18:08:09 -0500

    BISWAS

--- Test1.py
+++ Test1.py
@@ -11,6 +11,7 @@ def test_context_precision():
 
 
 # Create objects of the class for the specified matrix
+# power of LLM + method metrix -> Score
 context_precision = LLMContextPrecisionWithoutReference()
 
 # Feed Data 33 SDF
